4e8b60ff5bc510f8fb8c110c58ea715215740cbb
aniellonigrocogliano <aniello.nigro.cogliano@gmail.com>
2024-07-12 18:20:12 +0200

    add: flatcontroller

---
+++ app/Http/Controllers/Admin/FlatController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class FlatController extends Controller
+{
+    public function index() {}
+
+    public function show(string $slug) {}
+
+    public function create() {}
+
+    public function store(Request $request) {}
+
+    public function edit() {}
+
+    public function update(Request $request) {}
+}
