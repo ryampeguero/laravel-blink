18e9f7646c94c86cf2b2513280b156d73facdcf4
MattiaMessana <mattia.messana@icloud.com>
2024-07-12 16:45:16 +0200

    add : ciao

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,6 +12,7 @@
         </div>
         <h1 class="display-5 fw-bold">
             Welcome to Laravel+Bootsrap 5
+            ciao
         </h1>
 
         <p class="col-md-8 fs-4">Using a series of utilities, you can create this jumbotron, just like the one in previous versions of Bootstrap. Check out the examples below for how you can remix and restyle it to your liking.</p>
