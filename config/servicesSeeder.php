7c4dad525c16d75ea367644cd62a6d21c246d194
MattiaMessana <mattia.messana@icloud.com>
2024-07-12 16:32:06 +0200

    feat: create servicesSeeder.php in config and create ServicesTableSeeder

---
+++ config/servicesSeeder.php
@@ -0,0 +1,45 @@
+<?php
+return  [
+    [
+        "name" => "Wi-Fi",
+        "icon" => "fa-wifi"
+    ],
+    [
+        "name" => "Pulizia settimanale",
+        "icon" => "fa-broom"
+    ],
+    [
+        "name" => "Aria condizionata",
+        "icon" => "fa-snowflake"
+    ],
+    [
+        "name" => "Lavanderia",
+        "icon" => "fa-tshirt"
+    ],
+    [
+        "name" => "Piscina",
+        "icon" => "fa-swimmer"
+    ],
+    [
+        "name" => "Palestra",
+        "icon" => "fa-dumbbell"
+    ],
+    [
+        "name" => "Parcheggio",
+        "icon" => "fa-parking"
+    ],
+    [
+        "name" => "TV via cavo",
+        "icon" => "fa-tv"
+    ],
+    [
+        "name" => "Animali ammessi",
+        "icon" => "fa-paw"
+    ],
+    [
+        "name" => "Servizio di portineria",
+        "icon" => "fa-concierge-bell"
+    ]
+];
+
+?>
