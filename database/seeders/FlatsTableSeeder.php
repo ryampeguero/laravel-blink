1e4eb960bc6ddd71ce8b04922af7f56703e2a289
ryampeguero <ryampeguero@gmail.com>
2024-07-12 16:10:09 +0200

    fix: FlatsTableSeeder with new features

--- database/seeders/FlatsTableSeeder.php
+++ database/seeders/FlatsTableSeeder.php
@@ -7,7 +7,6 @@ use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Str;
-
 use Faker\Factory as Faker;
 
 class FlatsTableSeeder extends Seeder
@@ -21,8 +20,18 @@ class FlatsTableSeeder extends Seeder
         foreach ($usersArray as $user) {
             $faker = Faker::create("it_IT");
             $newFlat = new Flat();
+            $newFlat->name = $faker->word();
             $newFlat->rooms = $faker->numberBetween(1, 20);
-            $newFlat->slug = Str::slug();
+            $newFlat->bathrooms = $faker->numberBetween(1, 20);
+            $newFlat->beds = $faker->numberBetween(1, 20);
+            $newFlat->address = $faker->streetAddress();
+            $newFlat->latitude = $faker->numberBetween(1000, 5000);
+            $newFlat->longitude = $faker->numberBetween(1000, 5000);
+            $newFlat->visible = $faker->numberBetween(0, 1);
+            $slug = strval($newFlat->rooms);
+            $newFlat->slug = Str::slug($newFlat->name . $slug);
+            $newFlat->user_id = $user->id;
+            $newFlat->save();
         }
     }
 }
