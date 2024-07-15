98eec99b16166a4ac1729846675a5c358586f84b
aniellonigrocogliano <aniello.nigro.cogliano@gmail.com>
2024-07-15 12:11:24 +0200

    add: function create in flat controller

--- app/Http/Controllers/Admin/FlatController.php
+++ app/Http/Controllers/Admin/FlatController.php
@@ -3,19 +3,27 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Flat;
+use App\Models\Service;
 use Illuminate\Http\Request;
 
 class FlatController extends Controller
 {
     public function index() {}
 
-    public function create() {}
+    public function create()
+    {
+        $flat = Flat::all();
+        $service = Service::all();
+
+        return view(' admin.flats.create', compact('flat', 'service'));
+    }
 
     public function store(Request $request) {}
 
     public function show(string $slug) {}
 
     public function edit() {}
-    
+
     public function update(Request $request) {}
 }
--- app/Models/Flat.php
+++ app/Models/Flat.php
@@ -5,25 +5,34 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-
 class Flat extends Model
 {
     use HasFactory;
 
-    public function views () {
+    protected $fillable = ['id', 'slug', 'rooms', 'bathrooms', 'beds', 'square_meters', 'address', 'latitude', 'longitude', 'img_path', 'visible'];
+
+    public function views()
+    {
         return $this->hasMany(View::class);
     }
-    public function messages () {
+
+    public function messages()
+    {
         return $this->hasMany(Message::class);
     }
-    public function services () {
+
+    public function services()
+    {
         return $this->belongsToMany(Service::class);
     }
-    public function plans () {
+
+    public function plans()
+    {
         return $this->belongsToMany(Plan::class, 'receipts');
     }
-    public function user() {
+
+    public function user()
+    {
         return $this->belongsTo(User::class);
     }
-
 }
