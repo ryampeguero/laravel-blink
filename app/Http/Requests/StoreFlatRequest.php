0aaec3a990b5b7ce4787cbfeb3ed15a05115fd6a
aniellonigrocogliano <aniello.nigro.cogliano@gmail.com>
2024-07-15 17:33:08 +0200

    add:storeFlatRequest

---
+++ app/Http/Requests/StoreFlatRequest.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreFlatRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|string|min:3|max:255 ',
+            'rooms' => 'required|integer|not_in:0|gt:-1 ',
+            'bathrooms' => 'required|integer|not_in:0|gt:-1 ',
+            'beds' => 'required|integer|not_in:0|gt:-1 ',
+            'square_meter' => 'required|integer|not_in:0|gt:-1  ',
+            'address' => 'required|string|min:3|max:255',
+            'latitude' => ' nullable',
+            'longitude' => 'nullable ',
+            'img_path' => 'nullable',
+            'visible' => 'required|integer',
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'name.required' => 'Il nome dell\'appartamento è obbligatorio',
+            'name.string' => 'Il nome dell\'appartamento deve essre composto da una stringa',
+            'name.min' => 'Il nome dell\'appartamento deve avere almeno 3 caratteri',
+            'name.max' => 'Il nome dell\'appartamento non può superare i 255 caratteri',
+            'rooms.required' => 'Il numero delle camere è obligatorio',
+            'rooms.integer' => 'Il numero delle camere deve esseere un numero intero',
+            'rooms.not_in' => 'Il numero delle camere non può essere 0',
+            'rooms.gt' => 'Il numero delle camere non può essere negativo',
+            'beds.required' => 'Il numero di letti è obligatorio',
+            'beds.integer' => 'Il numero di letti deve esseere un numero intero',
+            'beds.not_in' => 'Il numero di letti non può essere 0',
+            'beds.gt' => 'Il numero di letti non può essere negativo',
+            'bathrooms.required' => 'Il numero di bagni è obligatorio',
+            'bathrooms.integer' => 'Il numero di bagni deve esseere un numero intero',
+            'bathrooms.not_in' => 'Il numero di bagni non può essere 0',
+            'bathrooms.gt' => 'Il numero di bagni non può essere negativo',
+            'square_meter.required' => 'I metri quadri è obligatorio',
+            'square_meter.integer' => 'I metri quadri deve esseere un numero intero',
+            'square_meter.not_in' => 'I metri quadri non può essere 0',
+            'square_meter.gt' => 'I metri quadri non può essere negativo',
+            'address.required' => 'L\'indirizzo è obbligatorio',
+            'address.string' => 'L\'indirizzo deve essre composto da una stringa',
+            'address.min' => 'L\'indirizzo deve avere almeno 3 caratteri',
+            'address.max' => 'L\'indirizzo non può superare i 255 caratteri',
+            'visible' => 'Devi scegliere se pubblicare o no la camera',
+            'visible' => 'Valore non valido',
+
+        ];
+    }
+}
