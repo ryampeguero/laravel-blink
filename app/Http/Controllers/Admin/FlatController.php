7bb75130ccaa5c10b61651cb0d7750dadfdb48f2
ryampeguero <ryampeguero@gmail.com>
2024-07-15 09:08:17 +0200

    fix: a few issues

--- app/Http/Controllers/Admin/FlatController.php
+++ app/Http/Controllers/Admin/FlatController.php
@@ -9,13 +9,13 @@ class FlatController extends Controller
 {
     public function index() {}
 
-    public function show(string $slug) {}
-
     public function create() {}
 
     public function store(Request $request) {}
 
-    public function edit() {}
+    public function show(string $slug) {}
 
+    public function edit() {}
+    
     public function update(Request $request) {}
 }
