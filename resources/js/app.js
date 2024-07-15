acb27d96e66adf732545039394a4e00d25004cf9
ChristianIlliano <grafica.illiano@gmail.com>
2024-07-15 18:50:59 +0200

    Add: tr clickable

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,3 +2,19 @@ import "./bootstrap";
 import "~resources/scss/app.scss";
 import.meta.glob(["../img/**"]);
 import * as bootstrap from "bootstrap";
+
+const test = document.querySelectorAll(".test");
+const urlShow = "http://127.0.0.1:8000/admin/flats/"
+let curValue = "";
+
+test.forEach((curItem)=>{
+    
+    curValue = curItem.firstElementChild.value;
+    console.log(curValue);
+    
+    curItem.addEventListener("click", ()=>{
+
+        document.location.href = `${urlShow}${curValue}`
+    } )
+    console.log(curItem);
+})
--- resources/views/admin/flats/index.blade.php
+++ resources/views/admin/flats/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <div class="ms_shadow mt-4 container ms_border p-4 ">
-        <div class=" mb-5 d-flex justify-content-between align-items-center ">
+        <div class=" mb-5 d-flex justify-content-between align-items-center">
             <div>
                 <h1>Appartamenti</h1>
             </div>
@@ -10,13 +10,13 @@
                 <a class="ms_button" href="">&plus; Aggiungi</a>
             </div>
         </div>
-        <div class="d-flex justify-content-between mb-3 gap-3 ">
+        <div class="justify-content-between mb-3 gap-3 stats">
             <div class="container-fluid ms_border_inner p-4 ms_shadow">
                 <h4>Stats</h4>
                 <h1>5264</h1>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
             </div>
-            <div class="container-fluid ms_border_inner p-4 ms_shadow" >
+            <div class="container-fluid ms_border_inner p-4 ms_shadow">
                 <h4>Stats</h4>
                 <h1>1820</h1>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
@@ -30,27 +30,28 @@
         <table class="ms_table">
             <thead>
                 <tr class="ms_tr">
-                    <th scope="col">Id</th>
+                    <th class="tabs" scope="col">Id</th>
                     <th scope="col">Nome</th>
                     <th scope="col">N° Stanze</th>
-                    <th scope="col">N° Bagni</th>
+                    <th class="tabs" scope="col">N° Bagni</th>
                     <th scope="col">N° Letti</th>
-                    <th scope="col">Indirizzo</th>
-                    <th scope="col"></th>
+                    <th class="tabs" scope="col">Indirizzo</th>
+                    <th class="tabs" scope="col"></th>
                 </tr>
             </thead>
             <tbody>
                 @foreach ($flatsArray as $item)
-                    <tr class="ms_shadow2">
-                        <th scope="row">{{ $item->id }}</th>
-                        <td>{{ $item->name }}</td>
-                        <td>{{ $item->rooms }}</td>
-                        <td>{{ $item->bathrooms }}</td>
-                        <td>{{ $item->beds }}</td>
-                        <td>{{ $item->address }}</td>
-                        <td>
-                            <a href="{{ route("admin.flats.show", ["flat"=>$item->slug]) }}"> ciao</a>
-                        </td>
+                    <tr class="ms_shadow2 test" data-href="{{ route('admin.flats.show', ['flat' => $item->slug]) }}">
+                        <input type="hidden" value="{{ $item->slug }}">
+                            <th class="tabs" scope="row">{{ $item->id }}</th>
+                            <td>{{ $item->name }}</td>
+                            <td>{{ $item->rooms }}</td>
+                            <td class="tabs">{{ $item->bathrooms }}</td>
+                            <td>{{ $item->beds }}</td>
+                            <td class="tabs">{{ $item->address }}</td>
+                            <td class="tabs">
+                                <a href="{{ route('admin.flats.show', ['flat' => $item->slug]) }}">INFO</a>
+                            </td>
                     </tr>
                 @endforeach
             </tbody>
