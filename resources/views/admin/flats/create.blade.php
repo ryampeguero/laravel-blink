dc42e5208f302ea826ce4be9251631785e2cd418
ryampeguero <ryampeguero@gmail.com>
2024-07-17 09:24:48 +0200

    feat: improved some style issues

--- resources/views/admin/flats/create.blade.php
+++ resources/views/admin/flats/create.blade.php
@@ -1,94 +1,154 @@
 @extends('layouts.admin')
 
 @section('content')
-    <div class="container">
+    <div class="container p-5">
         <h1>Aggiungi un nuovo appartamento</h1>
-        <form action="{{ route('admin.flats.store') }}" method="POST" enctype="multipart/form-data">
-            @csrf
+        <div class="container ms_shadow mt-4 container ms_border p-4">
+            <div class="row">
+                <div class="col-6">
+                    <form id='form-create' action="{{ route('admin.flats.store') }}" method="POST"
+                        enctype="multipart/form-data">
+                        <div class="container">
+                            @csrf
+                            <div class="row mb-3">
+                                <div class="col ">
+                                    <div class="">
+                                        <label for="name">Nome</label>
+                                        <input type="text" id="name" name="name"
+                                            class="form-control @error('name') is-invalid @enderror"
+                                            value="{{ old('name') }}">
+                                        @error('name')
+                                            <div class="invalid-feedback">{{ $message }}</div>
+                                        @enderror
+                                    </div>
+                                </div>
+                                <div class="col">
+                                    <div class="">
+                                        <label for="rooms">Numero di stanze</label>
+                                        <input type="number" id="rooms" name="rooms"
+                                            class="form-control @error('rooms') is-invalid @enderror"
+                                            value="{{ old('rooms') }}">
+                                        @error('rooms')
+                                            <div class="invalid-feedback">{{ $message }}</div>
+                                        @enderror
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row mb-3">
+                                <div class="col-6">
+                                    <div class="">
+                                        <label for="bathrooms">Numero di bagni</label>
+                                        <input type="number" id="bathrooms"
+                                            name="bathrooms"class="form-control @error('bathrooms') is-invalid @enderror"
+                                            value="{{ old('bathrooms') }}">
+                                        @error('bathrooms')
+                                            <div class="invalid-feedback">{{ $message }}</div>
+                                        @enderror
+                                    </div>
+                                </div>
 
-            <label for="name">Nome</label><br>
-            <input type="text" id="name" name="name" class="form-control @error('name') is-invalid @enderror"
-                value="{{ old('name') }}">
-            @error('name')
-                <div class="invalid-feedback">{{ $message }}</div>
-            @enderror
-            <br>
+                                <div class="col-6">
+                                    <div class="">
+                                        <label for="beds">Numero di letti</label>
+                                        <input type="number" id="beds"
+                                            name="beds"class="form-control @error('beds') is-invalid @enderror"
+                                            value="{{ old('beds') }}">
+                                        @error('beds')
+                                            <div class="invalid-feedback">{{ $message }}</div>
+                                        @enderror
 
-            <label for="rooms">Numero di stanze</label><br>
-            <input type="number" id="rooms" name="rooms"class="form-control @error('rooms') is-invalid @enderror"
-                value="{{ old('rooms') }}">
-            @error('rooms')
-                <div class="invalid-feedback">{{ $message }}</div>
-            @enderror
-            <br>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row mb-3">
+                                <div class="col">
+                                    <div class="">
+                                        <label for="square_meters">Metri quadri</label>
+                                        <input type="number" id="square_meters" name="square_meters"
+                                            class="form-control @error('square_meters') is-invalid @enderror"
+                                            value="{{ old('square_meters') }}">
+                                        @error('square_meters')
+                                            <div class="invalid-feedback">{{ $message }}</div>
+                                        @enderror
+                                    </div>
+                                </div>
+                            </div>
 
-            <label for="bathrooms">Numero di bagni</label><br>
-            <input type="number" id="bathrooms"
-                name="bathrooms"class="form-control @error('bathrooms') is-invalid @enderror"
-                value="{{ old('bathrooms') }}">
-            @error('bathrooms')
-                <div class="invalid-feedback">{{ $message }}</div>
-            @enderror
-            <br>
+                            <div class="row">
+                                <div class="col-6">
+                                    <div class="">
+                                        <label for="address">Indirizzo</label>
+                                        <input type="text" id="address"
+                                            name="address"class="form-control @error('address') is-invalid @enderror"
+                                            value="{{ old('address') }}">
+                                        @error('address')
+                                            <div class="invalid-feedback">{{ $message }}</div>
+                                        @enderror
+                                    </div>
+                                </div>
+                                <div id="map-create" class="col-6">
+                                    <div id="map" class="map"></div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col">
+                                    <div class="btn-group" role="group">
+                                        <div class="container">
+                                            <div class="row">
+                                                @foreach ($services as $service)
+                                                    <div class="col-3">
+                                                            
+                                                            @if (old('services') != null)
+                                                            <input @checked(in_array($service->id, old('services', []))) value="{{ $service->id }}"
+                                                            class="btn-check" type="checkbox" name="services[]"
+                                                            id="{{ $service->id }}">
+                                                            @else
+                                                            <input value="{{ $service->id }}" class="btn-check"
+                                                            type="checkbox" name="services[]" id="{{ $service->id }}">
+                                                            @endif
+                                                            <label style="font-size: 0.8rem"
+                                                                class="form-label btn btn-outline-primary test"
+                                                                for="{{ $service->id }}">{{ $service?->name }}</label>
+                                                    </div>
+                                                @endforeach
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
 
-            <label for="beds">Numero di letti</label><br>
-            <input type="number" id="beds" name="beds"class="form-control @error('beds') is-invalid @enderror"
-                value="{{ old('beds') }}">
-            @error('beds')
-                <div class="invalid-feedback">{{ $message }}</div>
-            @enderror
-            <br>
+                            <div class="row mb-3">
+                                <div class="col">
+                                    <label class="form-label" for="img_path">Immagine</label>
+                                    <input class="form-control" type="file" id="img_path" name="img_path">
+                                </div>
+                            </div>
 
-            <label for="square_meters">Metri quadri</label><br>
-            <input type="number" id="square_meters" name="square_meters"
-                class="form-control @error('square_meters') is-invalid @enderror" value="{{ old('square_meters') }}">
-            @error('square_meters')
-                <div class="invalid-feedback">{{ $message }}</div>
-            @enderror
-            <br>
+                            <div class="row mb-3">
+                                <div class="col prova">
+                                    <div class="">
+                                        <label for="address">Vuoi mettere in vendita subito il tuo
+                                            appartamento</label><br>
+                                        <label for="true">Si, voglio inserirlo ora</label>
+                                        <input type="radio" id="true" name="visible" value="1">
+                                    </div>
+                                    <div class="">
+                                        <label for="false">No, provvedrò dopo</label>
+                                        <input type="radio" id="false" name="visible" value="0">
+                                    </div>
+                                </div>
+                            </div>
 
-            <label for="address">Indirizzo</label><br>
-            <input type="text" id="address" name="address"class="form-control @error('address') is-invalid @enderror"
-                value="{{ old('address') }}">
-            @error('address')
-                <div class="invalid-feedback">{{ $message }}</div>
-            @enderror
-            <br>
-
-            <label for="latitude">latitude</label><br>
-            <input type="text" id="latitude" name="latitude"class="form-control @error('latitude') is-invalid @enderror"
-                value="{{ old('latitude') }}">
-            @error('latitude')
-                <div class="invalid-feedback">{{ $message }}</div>
-            @enderror
-            <br>
-
-            <label for="longitude">longitude</label><br>
-            <input type="text" id="longitude"
-                name="longitude"class="form-control @error('longitude') is-invalid @enderror"
-                value="{{ old('longitude') }}">
-            @error('longitude')
-                <div class="invalid-feedback">{{ $message }}</div>
-            @enderror
-            <br>
-
-            <h6>Seleziona i servizi disponibili</h6>
-            @foreach ($service as $service)
-                <input type="checkbox" id="service_{{ $service->id }}" name="services[]" value="{{ $service->id }}">
-                <label for="services[]"> {{ $service->name }}</label>
-            @endforeach
-            <br> <label for="img_path">Imagine</label><br>
-            <input type="file" id="img_path" name="img_path"><br>
-
-            <br> <label for="address">Vuoi mettere in vendita subito il tuo appartamento</label><br>
-            <input type="radio" id="true" name="visible" value="1">
-            <label for="true">Si, voglio inserirlo ora</label><br>
-            <input type="radio" id="false" name="visible" value="0">
-            <label for="false">No, provvedrò dopo</label><br>
-
-
-            <br> <button type="submit">Aggiungi appartamento</button><br>
-            <a href="{{ route('admin.dashboard') }}">Annulla</a>
-        </form>
+                            <div class="row">
+                                <div class="col">
+                                    <button class="btn btn-primary" type="submit">Aggiungi appartamento</button>
+                                    <a class="btn btn-danger" href="{{ route('admin.dashboard') }}">Annulla</a>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
     </div>
 @endsection
