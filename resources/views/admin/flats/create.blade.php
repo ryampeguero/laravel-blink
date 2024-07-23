404525b13355e9471dfc00706632599ee2d5d2f3
ryampeguero <ryampeguero@gmail.com>
2024-07-23 11:15:19 +0200

    fix: create view

--- resources/views/admin/flats/create.blade.php
+++ resources/views/admin/flats/create.blade.php
@@ -6,14 +6,16 @@
         <div class="row">
             <!-- Colonna del modulo -->
             <div class="col-xl-6 col-lg-7 col-md-12 mt-5">
-                <form class="px-4" id='form-create' action="{{ route('admin.flats.store') }}" method="POST" enctype="multipart/form-data" autocomplete="off">
+                <form class="px-4" id='form-create' action="{{ route('admin.flats.store') }}" method="POST"
+                    enctype="multipart/form-data" autocomplete="off">
                     @csrf
                     <div class="container-fluid p-0 m-0">
                         <!-- Nome -->
                         <div class="row mb-3">
                             <div class="col">
                                 <label for="name">Nome</label>
-                                <input type="text" id="name" name="name" class="form-control @error('name') is-invalid @enderror" value="{{ old('name') }}">
+                                <input type="text" id="name" name="name"
+                                    class="form-control @error('name') is-invalid @enderror" value="{{ old('name') }}">
                                 @error('name')
                                     <div class="invalid-feedback">{{ $message }}</div>
                                 @enderror
@@ -37,7 +39,8 @@
                             <div class="col-md-6">
                                 <label for="bathrooms">Numero di bagni</label>
                                 <div id='bathrooms' class="ms_input">
-                                    <input id='input_value' type="hidden" name="bathrooms" value="{{ old('bathrooms', '1') }}">
+                                    <input id='input_value' type="hidden" name="bathrooms"
+                                        value="{{ old('bathrooms', '1') }}">
                                     <button id="minus" class="input_btn">-</button>
                                     <span id="ms_value"></span>
                                     <button id="plus" class="input_btn">+</button>
@@ -49,14 +52,17 @@
                         <div class="row mb-3">
                             <div class="col-md-6">
                                 <label for="beds">Numero di letti</label>
-                                <input type="number" id="beds" name="beds" class="form-control @error('beds') is-invalid @enderror" value="{{ old('beds') }}">
+                                <input type="number" id="beds" name="beds"
+                                    class="form-control @error('beds') is-invalid @enderror" value="{{ old('beds') }}">
                                 @error('beds')
                                     <div class="invalid-feedback">{{ $message }}</div>
                                 @enderror
                             </div>
                             <div class="col-md-6">
                                 <label for="square_meters">Metri quadrati</label>
-                                <input type="number" id="square_meters" name="square_meters" class="form-control @error('square_meters') is-invalid @enderror" value="{{ old('square_meters') }}">
+                                <input type="number" id="square_meters" name="square_meters"
+                                    class="form-control @error('square_meters') is-invalid @enderror"
+                                    value="{{ old('square_meters') }}">
                                 @error('square_meters')
                                     <div class="invalid-feedback">{{ $message }}</div>
                                 @enderror
@@ -67,7 +73,8 @@
                         <div class="row mb-3">
                             <div class="col">
                                 <label class="form-label" for="address">Cerca Indirizzo</label>
-                                <input value="{{ old('address') }}" name="address" class="form-control" type="text" id="address" autocomplete="off">
+                                <input value="{{ old('address') }}" name="address" class="form-control" type="text"
+                                    id="address" autocomplete="off">
                                 <input type="hidden" name="latitude" id="latitude">
                                 <input type="hidden" name="longitude" id="longitude">
                                 <ul id="suggestions"></ul>
@@ -85,7 +92,7 @@
                         <!-- Optional -->
                         <div class="row mb-3">
                             <div class="col">
-                                <h4>Filtra gli optional della tua stanza</h4>
+                                <h4>Optional</h4>
 
                                 <div class="btn-group" role="group">
                                     <div class="">
@@ -107,46 +114,46 @@
                                             @endforeach
 
                                         </div>
-                                    @endforeach
+                                    </div>
+                                </div>
+                            </div>
+
+                            <!-- Immagine -->
+                            <div class="row mb-3">
+                                <div class="col">
+                                    <label class="form-label" for="img_path">Immagine</label>
+                                    <input class="form-control" type="file" id="img_path" name="img_path">
+                                </div>
+                            </div>
+
+                            <!-- Visibilità -->
+                            <div class="row mb-3">
+                                <div class="col">
+                                    <label class="form-label" for="visible">Visibile*</label>
+                                    <select class="form-select" name="visible" id="visible">
+                                        <option value="">Seleziona</option>
+                                        <option @selected(old('visible') == '1') value="1">Si</option>
+                                        <option @selected(old('visible') == '0') value="0">No</option>
+                                    </select>
+                                </div>
+                            </div>
+
+                            <!-- Pulsanti -->
+                            <div class="row">
+                                <div class="col d-flex justify-content-between">
+                                    <a class="ms_button_secondary" href="{{ route('admin.dashboard') }}">Annulla</a>
+                                    <button class="ms_button" type="submit">Aggiungi appartamento</button>
                                 </div>
                             </div>
                         </div>
-
-                        <!-- Immagine -->
-                        <div class="row mb-3">
-                            <div class="col">
-                                <label class="form-label" for="img_path">Immagine</label>
-                                <input class="form-control" type="file" id="img_path" name="img_path">
-                            </div>
-                        </div>
-
-                        <!-- Visibilità -->
-                        <div class="row mb-3">
-                            <div class="col">
-                                <label class="form-label" for="visible">Visibile*</label>
-                                <select class="form-select" name="visible" id="visible">
-                                    <option value="">Seleziona</option>
-                                    <option @selected(old('visible') == '1') value="1">Si</option>
-                                    <option @selected(old('visible') == '0') value="0">No</option>
-                                </select>
-                            </div>
-                        </div>
-
-                        <!-- Pulsanti -->
-                        <div class="row">
-                            <div class="col d-flex justify-content-between">
-                                <a class="ms_button_secondary" href="{{ route('admin.dashboard') }}">Annulla</a>
-                                <button class="ms_button" type="submit">Aggiungi appartamento</button>
-                            </div>
-                        </div>
-                    </div>
                 </form>
             </div>
-
-            <!-- Colonna dell'immagine -->
-            <div class="col-xl-6 col-lg-5 col-md-12 mt-5">
-                <img class="ms_img ms_border_inner" src="{{ asset('img/placeholder_img_fit.png') }}" alt="">
-            </div>
         </div>
+
+        <!-- Colonna dell'immagine -->
+        <div class="col-xl-6 col-lg-5 col-md-12 mt-5">
+            <img class="ms_img ms_border_inner" src="{{ asset('img/placeholder_img_fit.png') }}" alt="">
+        </div>
+    </div>
     </div>
 @endsection
